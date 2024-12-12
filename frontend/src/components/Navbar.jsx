66fc8076e6a313301eee6502870a7913fb70cdf1
MehdiBenameur <mohamedmehdi.benameur@polytechnicien.tn>
2024-12-12 17:01:01 +0100

    Project done

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,48 +1,47 @@
 import { Button, Container, Flex, HStack, Text, useColorMode } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
-import {PlusSquareIcon} from "@chakra-ui/icons"
+
+import { PlusSquareIcon } from "@chakra-ui/icons";
 import { IoMoon } from "react-icons/io5";
 import { LuSun } from "react-icons/lu";
 
-
 const Navbar = () => {
-  const { colorMode, toggleColorMode} = useColorMode();
-  return (
-    <Container maxH={"1140px"} px={4} >
-      <Flex
-      h={16}
-      alignItems={"center"}
-      justifyContent={"space-between"}
-      flexDir={{
-        base:"column",
-        sm:"row"
-      }}
-      >
-        <Text
-        fontSize={{base: "22", sm: "28"}}
-        fontWeight={"bold"}
-        textTransform={"uppercase"}
-        textAlign={"center"}
-        bgGradient={"linear(to-r, cyan.400, blue.500)"}
-        bgClip={"text"}
-        >
-         <Link to={"/"}>Product Store 🛒</Link> 
+	const { colorMode, toggleColorMode } = useColorMode();
 
-        </Text>
-        <HStack spacing={2} alignItems={"center"}>
-          <Link to={"/create"} >
-          <Button>
-            <PlusSquareIcon fontSize={20} />
-          </Button>
-          </Link>
-          <Button onClick={toggleColorMode}>
-            {colorMode === "light" ? <IoMoon /> : <LuSun size='20' />}
+	return (
+		<Container maxW={"1140px"} px={4}>
+			<Flex
+				h={16}
+				alignItems={"center"}
+				justifyContent={"space-between"}
+				flexDir={{
+					base: "column",
+					sm: "row",
+				}}
+			>
+				<Text
+					fontSize={{ base: "22", sm: "28" }}
+					fontWeight={"bold"}
+					textTransform={"uppercase"}
+					textAlign={"center"}
+					bgGradient={"linear(to-r, cyan.400, blue.500)"}
+					bgClip={"text"}
+				>
+					<Link to={"/"}>Product Store 🛒</Link>
+				</Text>
 
-          </Button>
-        </HStack>
-      </Flex>
-    </Container>
-  )
-}
-
+				<HStack spacing={2} alignItems={"center"}>
+					<Link to={"/create"}>
+						<Button>
+							<PlusSquareIcon fontSize={20} />
+						</Button>
+					</Link>
+					<Button onClick={toggleColorMode}>
+						{colorMode === "light" ? <IoMoon /> : <LuSun size='20' />}
+					</Button>
+				</HStack>
+			</Flex>
+		</Container>
+	);
+};
 export default Navbar;
